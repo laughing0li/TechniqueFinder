0ee214d95704494c4ff23e4fb041ef11cc88a424
Vincent Fazio <vjf@users.noreply.github.com>
2021-08-01 23:01:17 +0000

    Swap model & instrument type columns in technique list

--- application/views/Portal/technique_list.php
+++ application/views/Portal/technique_list.php
@@ -1,3 +1,17 @@
+<?php
+/**
+ * TechniqueFinder - technique_list.php
+ *
+ * Description:
+ * Author:           Intersect Australia Ltd
+ * Created:          12 Aug 2019
+ * Source:           https://github.com/IntersectAustralia/TechniqueFinder
+ * License:          Copyright (c) 2019 Intersect Australia - Licensed under Creative Commons
+ *                   Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+ *                   https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+?>
+
 <?php $this->load->view('layout/portal_header.php'); ?>
 <head><title>Technique List</title></head>
 <body>
@@ -12,27 +26,15 @@
                     <table class="table table-striped">
                         <thead>
                         <tr class="tf-font-14">
-                            <th scope="col">Model</th>
                             <th scope="col">Instrument Type</th>
+                            <th scope="col">Model</th>
                             <th scope="col">Manufacturer</th>
                         </tr>
                         </thead>
                         <tbody>
                         <?php
-                        /**
-                         * TechniqueFinder - technique_list.php
-                         *
-                         * Description:
-                         * Author:           Intersect Australia Ltd
-                         * Created:          12 Aug 2019
-                         * Source:           https://github.com/IntersectAustralia/TechniqueFinder
-                         * License:          Copyright (c) 2019 Intersect Australia - Licensed under Creative Commons
-                         *                   Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
-                         *                   https://creativecommons.org/licenses/by-nc-sa/4.0/
-                         */
-
                         foreach ($allTechniques as $r) {
-                            echo '<tr class="tf-font-14"><th scope="row"><a href="' . base_url() . 'Portal/viewTechnique/' . $r->id . '?nav_from=listTechniques" >' . $r->model . '</a></th><td>' . $r->instrument_name . '</td><td>' . $r->manufacturer . '</td></tr>';
+                            echo '<tr class="tf-font-14"><th scope="row"><a href="' . base_url() . 'Portal/viewTechnique/' . $r->id . '?nav_from=listTechniques" >' . $r->instrument_name . '</a></th><td>' . $r->model . '</td><td>' . $r->manufacturer . '</td></tr>';
                         }
                         ?>
                         </tbody>
