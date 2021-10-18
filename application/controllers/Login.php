e4b71b719dd8cad25b8d194464fe4a6a85be42fe
YunLong <ylong.au@gmail.com>
2021-10-18 08:47:05 +0800

    finish auth0 function

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -29,21 +29,19 @@ class Login extends CI_Controller
     public function setupAuth0(): void
     {
         $this->auth0 = new Auth0([
-            // 'domain' => 'dev-68ut2myz.us.auth0.com',
-            // 'client_id' => 'ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf',
-            // 'client_secret' => 'XUpVeA315JxtukFrmSaAwLUj4oLiG0R10eogxef701-k6ucwXrfm4rfCkKOvuy8N',
-            // 'redirect_uri' => 'https://dt-test-326006.ts.r.appspot.com/callback',
-            // 'redirect_uri' => 'http://web.local:8080/callback',
-            'domain' => 'dev-csiro.au.auth0.com',
-            'client_id' => 'kWEhhcCcuFlF5QZsIVnocssVFZ9oFhrM',
-            'client_secret' => 'Vd0Z_zbcIdR9vrpys3ihrXHJlmSdJfAU8JihpC0AHWFz4AqZbvi09l91fgKBtCx2',
-            'redirect_uri' => 'https://labfinder.geoanalytics.group/callback',
+            'domain' => 'dev-68ut2myz.us.auth0.com',
+            'client_id' => 'ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf',
+            'client_secret' => 'XUpVeA315JxtukFrmSaAwLUj4oLiG0R10eogxef701-k6ucwXrfm4rfCkKOvuy8N',
+            'redirect_uri' => 'http://web.local:8080/callback',
+            // 'domain' => 'dev-csiro.au.auth0.com',
+            // 'client_id' => 'kWEhhcCcuFlF5QZsIVnocssVFZ9oFhrM',
+            // 'client_secret' => 'Vd0Z_zbcIdR9vrpys3ihrXHJlmSdJfAU8JihpC0AHWFz4AqZbvi09l91fgKBtCx2',
+            // 'redirect_uri' => 'https://labfinder.geoanalytics.group/callback',
         ]);
     }
 
     public function onLoginRoute()
     {
-
         $this->auth0->deleteAllPersistentData();
         $url = $this->auth0->login();
         header('Location: ' . $url, true, 303);
@@ -52,7 +50,19 @@ class Login extends CI_Controller
     public function onCallbackRoute()
     {
         $this->auth0->exchange();
-        header('Location: ' . '/TechniqueFinder/index', true, 303);
+        $user = $this->session->userdata('auth0__user');
+        if ($user['email_verified'] && $user['email'] == 'laughinglyl90@gmail.com') {
+            header('Location: ' . '/TechniqueFinder/index', true, 303);
+        } else {
+            $this->session->sess_destroy();
+            $this->auth0->logout();
+            $this->auth0->deleteAllPersistentData();
+            echo '<script>let r=alert("You do not have the access permit, Please contact the admin")</script>';
+            echo '<script> if (r == true) {</script>';
+            echo '<script>window.location.href="https://dev-68ut2myz.us.auth0.com/v2/logout?client_id=ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf&returnTo=http://web.local:8080/Portal"</script>';
+            // echo '<script>window.location.href="https://dev-csiro.au.auth0.com/v2/logout?client_id=kWEhhcCcuFlF5QZsIVnocssVFZ9oFhrM&returnTo=https://labfinder.geoanalytics.group/Portal"</script>';
+            echo '<script>}</script>';
+        }
     }
 
 
@@ -61,9 +71,9 @@ class Login extends CI_Controller
         $this->session->sess_destroy();
         $this->auth0->logout();
         $this->auth0->deleteAllPersistentData();
-        // redirect('https://dev-68ut2myz.us.auth0.com/v2/logout?client_id=ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf&returnTo=https://dt-test-326006.ts.r.appspot.com/Portal');
-        // redirect('https://dev-68ut2myz.us.auth0.com/v2/logout?client_id=ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf&returnTo=http://web.local:8080/Portal');
-        redirect('https://dev-csiro.au.auth0.com/v2/logout?client_id=kWEhhcCcuFlF5QZsIVnocssVFZ9oFhrM&returnTo=https://labfinder.geoanalytics.group/Portal');
+        redirect('https://dev-68ut2myz.us.auth0.com/v2/logout?client_id=ubyDQgyXX9U4vQSETnCmgzQHB8PLQWEf&returnTo=http://web.local:8080/Portal');
+        // labfinder
+        // redirect('https://dev-csiro.au.auth0.com/v2/logout?client_id=kWEhhcCcuFlF5QZsIVnocssVFZ9oFhrM&returnTo=https://labfinder.geoanalytics.group/Portal');
     }
 
 }
