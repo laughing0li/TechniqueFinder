d8314228d2b051f99eaa1efad3a98546d59ce723
YunLong <ylong.au@gmail.com>
2021-11-08 13:40:47 +0800

    fix the error of add media

--- application/controllers/Localisation.php
+++ application/controllers/Localisation.php
@@ -6,14 +6,14 @@ class Localisation extends CI_Controller
     function __construct()
     {
         parent::__construct();
-        if (!($this->session->userdata('logged_in') == True)) {
-            redirect(base_url() . 'login/index');
-        }
+        // if (!($this->session->userdata('logged_in') == True)) {
+        //     redirect(base_url() . 'login/index');
+        // }
 
         // auth0 config
-        // if ($this->session->userdata('auth0__user') == null){
-        //     redirect(base_url() . 'authLogin');
-        // }
+        if ($this->session->userdata('auth0__user') == null){
+            redirect(base_url() . 'authLogin');
+        }
         
         $this->load->model('Localisation_model');
 
