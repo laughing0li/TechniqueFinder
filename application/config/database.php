f0cf19d0d4493c3e6737fedfdfc57988643723b5
YunLong <ylong.au@gmail.com>
2022-01-10 15:00:48 +0800

    set up github actions

--- application/config/database.php
+++ application/config/database.php
@@ -77,7 +77,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => $_ENV['HOSTNAME'],
+	'hostname' => getenv('HOSTNAME'),
 	'username' => $_ENV['USERNAME'],
 	'password' => $_ENV['PASSWORD'],
 	'database' => $_ENV['DATABASE'],
